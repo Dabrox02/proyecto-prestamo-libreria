99e495e6cde5e185d0d705f59df90b0fa1699de2
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-09-28 06:46:31 -0500

    feat: :sparkles: add methods put and delete

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const URI = "http://localhost:5050/libros";
+const URI_LIBROS = "http://localhost:5050/libros";
 
 const getData = async (uri) => {
     return await (await fetch(uri)).json();
@@ -12,10 +12,23 @@ const postData = async ({ URI, body }) => {
     });
 }
 
+const putData = async ({ URI, body }) => {
+    return await fetch(URI, {
+        method: "PUT",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+    });
+}
+
+const delData = async ({ URI }) => {
+    return (await fetch(URI, {
+        method: "DELETE",
+    })).ok;
+}
+
 
 // OBTENER
-
-// console.log(await getData(URI));
+// console.log(await getData(URI_LIBROS));
 
 // AGREGAR
 // let res = await postData({
@@ -27,3 +40,15 @@ const postData = async ({ URI, body }) => {
 // });
 // console.log(res.ok ? "Agregado" : "Fallido");
 
+// ACTUALIZAR
+// let res = await putData({
+//     URI: URI_LIBROS + "/1",
+//     body: {
+//         "autor": "Cristian Lopez",
+//         "fecha": 2019,
+//     }
+// });
+// console.log(res.ok ? "Actualizado" : "Fallido");
+
+// ELIMINAR
+// console.log(await delData({URI: URI_LIBROS + "/4"}));
