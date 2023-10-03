879ad6fe489f82a5bc0130827ad458fdfcd52547
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-02 21:39:46 -0500

    feat: :sparkles: adapt interface to categoria.js

--- storage/categoria.js
+++ storage/categoria.js
@@ -22,13 +22,14 @@ export const getAll = async () => {
 }
 
 export const post = async (obj) => {
+    let body = {};
     try {
-        Object.entries(interfaz).forEach(e => isCampoValido({ campo: e[0], valor: obj[e[0]], tipoEsperado: e[1] }));
+        Object.entries(interfaz).forEach(e => Object.assign(body, isCampoValido({ campo: e[0], valor: obj[e[0]], tipoEsperado: e[1] })));
     } catch (e) {
         return { status: 400, message: e.message }
     }
     config.method = "POST";
-    config.body = JSON.stringify(obj);
+    config.body = JSON.stringify(body);
     let res = await (await fetch(`${uri}${endpoint}`, config)).json();
     return res;
 }
@@ -45,14 +46,15 @@ export const deleteOne = async (id) => {
 }
 
 export const putOne = async (obj = {}) => {
+    let body = {};
     try {
         isCampoValido({ campo: Object.keys(primaryKey)[0], valor: obj.id, tipoEsperado: Object.values(primaryKey)[0] });
-        Object.entries(interfaz).forEach(e => isCampoValido({ campo: e[0], valor: obj[e[0]], tipoEsperado: e[1] }));
+        Object.entries(interfaz).forEach(e => Object.assign(body, isCampoValido({ campo: e[0], valor: obj[e[0]], tipoEsperado: e[1] })));
     } catch (e) {
         return { status: 400, message: e.message }
     }
     config.method = "PUT";
-    config.body = JSON.stringify(obj);
+    config.body = JSON.stringify(body);
     let res = await (await fetch(`${uri}${endpoint}/${obj.id}`, config)).json();
     return res;
 }
