63c4a08a6e3a2d90361e99608cf339dba2844712
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-02 20:44:58 -0500

    fix: :bug: fix function to validate
    - fix (ce850786)

--- util/validaciones.js
+++ util/validaciones.js
@@ -1,5 +1,9 @@
 export const isCampoValido = ({ campo = "", valor, tipoEsperado }) => {
     if (valor === null || valor === undefined) throw new Error(`Campo: ${campo}. No esta definido`);
+    if (tipoEsperado == "date") {
+        valor = new Date(valor);
+        if (isNaN(valor.getTime())) throw new Error('La fecha es inválida.');
+    };
     if (valor.constructor.name.toLowerCase() !== tipoEsperado.toLowerCase()) throw new Error(`El campo ${campo} con valor ${valor} no corresponde al tipo de dato.`);
     return true;
 }
