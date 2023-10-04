12992c43b88dce3598bd7bcf684c209510adb519
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-03 20:57:00 -0500

    feat: :sparkles: add getOne function and fix put
    - add new function getOne
    - fix put with optionals parameters

--- storage/estado-libro.js
+++ storage/estado-libro.js
@@ -22,6 +22,18 @@ export const getAll = async () => {
     return res;
 }
 
+export const getOne = async (id) => {
+    try {
+        isCampoValido({ campo: Object.keys(primaryKey)[0], valor: id, tipoEsperado: Object.values(primaryKey)[0] });
+    } catch (e) {
+        return { status: 400, message: e.message }
+    }
+    config.method = "GET";
+    config.body = undefined;
+    let res = await (await fetch(`${uri}${endpoint}/${id}`, config)).json();
+    return res;
+}
+
 export const post = async (obj) => {
     let body = {};
     try {
@@ -48,19 +60,24 @@ export const deleteOne = async (id) => {
 }
 
 export const putOne = async (obj = {}) => {
-    let body = {};
+    let newData = {};
+    let oldData = {};
     try {
+        oldData = await getOne(obj.id);
         isObjectoValido(obj);
         isCampoValido({ campo: Object.keys(primaryKey)[0], valor: obj.id, tipoEsperado: Object.values(primaryKey)[0] });
-        Object.entries(interfaz).forEach(e => Object.assign(body, isCampoValido({ campo: e[0], valor: obj[e[0]], tipoEsperado: e[1] })));
+        Object.entries(interfaz).forEach(e => {
+            obj[e[0]] ? Object.assign(newData, isCampoValido({ campo: e[0], valor: obj[e[0]], tipoEsperado: e[1] })) : "";
+        })
     } catch (e) {
         return { status: 400, message: e.message }
     }
     config.method = "PUT";
-    config.body = JSON.stringify(body);
+    config.body = JSON.stringify({ ...oldData, ...newData });
     let res = await (await fetch(`${uri}${endpoint}/${obj.id}`, config)).json();
     return res;
 }
+
 
 // * POST DE PRUEBA
 // console.log(await post({
