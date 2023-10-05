88b583c3f849ac1600fa9d150030bf267154e5f2
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-05 06:54:21 -0500

    feat: :sparkles: modularize function isRelacionValida

--- util/validaciones.js
+++ util/validaciones.js
@@ -14,7 +14,11 @@ export const isObjectoValido = (obj) => {
     if (obj == undefined || obj.constructor.name !== "Object" || Object.entries(obj).length == 0) throw new Error(`No envio parametros correctos.`);
 }
 
-export const isRelacionValida = async ({ id, foreignKey, endpoint }) => {
-    let res = await (await fetch(`${env.uri}${endpoint}/${id}`)).json()
-    return (Object.entries(res).length == 0) ? { status: 400, message: `Valor ${id} del campo ${foreignKey}, no existe.` } : {}
+export const isRelacionValida = async ({ obj }) => {
+    for (const key of ["id_autor", "id_categoria", "id_editorial", "id_estado", "id_usuario", "id_libro"]) {
+        if (obj[key]) {
+            let valid = await (await fetch(`${env.uri}/${key.split("_")[1]}/${obj[key]}`)).json();
+            if (Object.entries(valid).length == 0) throw new Error(`Valor ${obj[key]} del campo ${key}, no existe.`);
+        }
+    }
 }
