42ea700652199e025423c311eae1857c71b5e4f4
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-05 22:51:04 -0500

    feat: :sparkles: add custom routes json-server

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const hostName = "127.0.0.1";
 const port = "5010";
 const ssl = "http://";
-const uri = `${ssl + hostName}:${port}`
+const uri = `${ssl + hostName}:${port}`;
 
 export default {
     hostName,
