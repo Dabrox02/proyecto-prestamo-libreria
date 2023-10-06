ba176533cc83d79bd851d4f160a0148c23064a8c
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-06 13:26:27 -0500

    feat: :construction: creating card-component

--- components/card-component/card-component.js
+++ components/card-component/card-component.js
@@ -8,14 +8,13 @@ export class CardComponent extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = /*html*/`
-        <div class="card">
+        <div class="card text-center" style="width: 15rem; height: 15rem;">
         <div class="card-header">
           <h5 class="m-0">${this.title}</h5>
         </div>
         <div class="card-body">
           <h6 class="card-title">ISBN: ${this.descripcion}</h6>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <p class="card-text"></p>
         </div>
       </div>
         `
