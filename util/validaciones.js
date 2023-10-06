92fb69f5b665e7c539edf062690098364c0f3f49
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-05 19:45:26 -0500

    feat: :sparkles: add new validation for get request

--- util/validaciones.js
+++ util/validaciones.js
@@ -4,14 +4,14 @@ export const isCampoValido = ({ campo = "", valor, tipoEsperado }) => {
     if (valor === null || valor === undefined) throw new Error(`Campo: ${campo}. No esta definido`);
     if (tipoEsperado == "date") {
         valor = new Date(valor);
-        if (isNaN(valor.getTime()) || !(valor.getFullYear() <= 2040)) throw new Error('La fecha es inválida.');
+        if (isNaN(valor.getTime()) || !(valor.getFullYear() <= 2040)) throw new Error(`La fecha es inválida.`);
     };
     if (valor.constructor.name.toLowerCase() !== tipoEsperado.toLowerCase()) throw new Error(`El campo ${campo} con valor ${valor} no corresponde al tipo de dato.`);
     return { [campo]: valor instanceof Date ? valor.toISOString().split('T')[0] : valor };
 }
 
 export const isObjectoValido = (obj) => {
-    if (obj == undefined || obj.constructor.name !== "Object" || Object.entries(obj).length == 0) throw new Error(`No envio parametros correctos.`);
+    if (obj == undefined || obj.constructor.name !== "Object" || Object.entries(obj).length == 0) throw new Error(`Parametros incorrectos.`);
 }
 
 export const isRelacionValida = async ({ obj }) => {
@@ -20,5 +20,12 @@ export const isRelacionValida = async ({ obj }) => {
             let valid = await (await fetch(`${env.uri}/${key.split("_")[1]}/${obj[key]}`)).json();
             if (Object.entries(valid).length == 0) throw new Error(`Valor ${obj[key]} del campo ${key}, no existe.`);
         }
+    }
+}
+
+export const isDataCompleta = async ({ obj }) => {
+    for (const e of obj) {
+        isObjectoValido(e);
+        await isRelacionValida({ obj: e });
     }
 }
