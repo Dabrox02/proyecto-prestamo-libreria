83a838d6e9996970cf4959532b96d91e4e4ac076
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-07 15:27:53 -0500

    feat: :sparkles: table of books done

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 import autor from "./api/storage/autor.js"
 import libro from "./api/storage/libro.js";
 import libros from "./api/storage/libro.js"
+import config from "./config.js";
 
+const d = document;
 
 export const app = async () => {
     let path = window.location.pathname.split(".")[0];
+    if (path === "/index") {
+        console.log(path);
+    }
 
-    if (path == "/views/libros") {
-        let container = document.querySelector("#grilla-libros");
-        let books = await libros.getAll();
-        console.log(container);
-        console.log(books);
-        books.forEach(e => {
-            container.insertAdjacentHTML("beforeend", `<card-component 
-            title="${e.titulo}"
-            descripcion="${e.isbn}"
-            >
-            </card-component>`)
+    if (path === "/views/libros") {
+        const imgTmp = `${config.uri}/assets/img/templateBook.png`;
+        const table = new DataTable('#dt-libros', {
+            "responsive": true, "lengthChange": true, "autoWidth": false
         });
+        let books = await libros.getAllDetails();
+
+        books.forEach((val, index) => {
+            table.row.add([
+                val.id,
+                val.titulo,
+                val.autore.nombre,
+                val.estado.nombre,
+                val.categoria.nombre,
+                val.editoriale.nombre,
+                val.fecha_publicacion,
+                /*html*/`<img src="${val.urlImg ? val.urlImg : imgTmp}" alt="${val.titulo}" class="img-thumbnail img-table">`
+            ]).draw()
+        })
     }
 
 }
