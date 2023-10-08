086b602c158e812cd4fc1cf937e11965765f0cb7
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-08 17:44:02 -0500

    feat: :sparkles: crud books done

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ import editorial from "./api/storage/editorial.js"
 import estado from "./api/storage/estado.js"
 import libro from "./api/storage/libro.js"
 import config from "./config.js";
-import { cargarTabla, cargarSelectsForm, agregarLibro, eliminarLibro } from "./modules/libro.js";
+import { cargarTabla, formAgregarLibro, agregarLibro, eliminarLibro, editarLibro, formEditarLibro } from "./modules/libro.js";
 
 const d = document;
+const $ = (e) => d.querySelector(e);
 
 export const app = async () => {
     let path = window.location.pathname.split(".")[0];
@@ -17,23 +18,59 @@ export const app = async () => {
     if (path === "/views/libros") {
         let fnGetLibros = libro.getAllDetails;
         cargarTabla({ url: config.uri, fnGetLibros });
-        let authors = await autor.getAll();
-        let categories = await categoria.getAll();
-        let editorials = await editorial.getAll();
-        let states = await estado.getAll();
-        cargarSelectsForm({ authors, categories, editorials, states })
 
         document.addEventListener("submit", async (e) => {
             e.preventDefault();
             if (e.target.matches("#add-book-form")) {
                 let data = Object.fromEntries(new FormData(e.target));
                 await agregarLibro({ data, fnPostLibro: libro.post });
+                e.target.closest("dialog").close();
+            }
+            if (e.target.matches("#edit-book-form")) {
+                let data = Object.fromEntries(new FormData(e.target));
+                await editarLibro({ data: { "id": e.target.dataset.edit, ...data }, fnPutLibro: libro.putOne });
+                e.target.closest("dialog").close();
             }
         })
 
         document.addEventListener("click", async (e) => {
+            if (e.target.matches(".btn-close-modal")) {
+                e.target.closest("dialog").close();
+            }
+
+            if (e.target.matches("#btn-add-book")) {
+                $("#modal-book").showModal();
+                $("#modal-book").innerHTML = "";
+                $("#modal-book").insertAdjacentHTML("beforeend", await formAgregarLibro({
+                    fnGetAuthors: autor.getAll,
+                    fnGetCategories: categoria.getAll,
+                    fnGetEditorials: editorial.getAll,
+                    fnGetStates: estado.getAll
+                }));
+            }
+
             if (e.target.matches("#btn-del-book")) {
                 await eliminarLibro({ id: e.target.dataset.del, fnDelLibro: libro.deleteOne });
+            }
+
+            if (e.target.matches("#btn-edit-book")) {
+                let editar = e.target.dataset.edit;
+                let book = await libro.getOne(Number(editar));
+                console.log(book);
+                $("#modal-book").showModal();
+                $("#modal-book").innerHTML = "";
+                $("#modal-book").insertAdjacentHTML("beforeend", await formEditarLibro({
+                    book,
+                    fnGetAuthors: autor.getAll,
+                    fnGetCategories: categoria.getAll,
+                    fnGetEditorials: editorial.getAll,
+                    fnGetStates: estado.getAll
+                }));
+                $("#category-book").value = book.categoriaId;
+                $("#author-book").value = book.autoreId;
+                $("#editorial-book").value = book.editorialeId;
+                $("#state-book").value = book.estadoId;
+
             }
         })
     }
--- modules/libro.js
+++ modules/libro.js
@@ -1,8 +1,8 @@
 import { swalAlert } from "../components/swal-alert/swal-alert-component.js"
 
 export const cargarTabla = async ({ url, fnGetLibros }) => {
-    let books = await fnGetLibros(); // Modo Slow
-    // let books = await (await fetch("http://127.0.0.1:5010/libros/details")).json();  // Modo Fast
+    // let books = await fnGetLibros(); // Modo Slow
+    let books = await (await fetch("http://127.0.0.1:5010/libros/details")).json();  // Modo Fast
     const imgTmp = `${url}/assets/img/templateBook.png`;
     const table = new DataTable('#dt-libros', {
         "responsive": true, "lengthChange": true, "autoWidth": false
@@ -23,42 +23,161 @@ export const cargarTabla = async ({ url, fnGetLibros }) => {
             <div class="functions">
                 <button id="btn-edit-book" class="btn btn-primary f-size-3" type="button" data-edit="${val.id}">Editar</button>
                 <button id="btn-del-book" class="btn btn-danger f-size-3" type="button" data-del="${val.id}">Eliminar</button>
-            </div>
-            `
-            ]).draw()
+            </div>`]).draw();
         })
     }
 }
 
-export const cargarSelectsForm = ({ categories, authors, states, editorials }) => {
-    let selectCategoria = document.querySelector("#category-book");
-    let selectAuthor = document.querySelector("#author-book");
-    let selectEditorial = document.querySelector("#editorial-book");
-    let selectState = document.querySelector("#state-book");
-
-    categories.forEach((e) => {
-        selectCategoria.insertAdjacentHTML("beforeend", /*html*/`
-        <option value="${e.id}">${e.nombre}</option>
-        `)
+const crearOptions = (elements) => {
+    return elements.map((e) => {
+        return /*html*/`<option value="${e.id}">${e.nombre}</option>`
     })
+}
 
-    authors.forEach((e) => {
-        selectAuthor.insertAdjacentHTML("beforeend", /*html*/`
-        <option value="${e.id}">${e.nombre}</option>
-        `)
-    })
+export const formAgregarLibro = async ({ fnGetAuthors, fnGetCategories, fnGetEditorials, fnGetStates }) => {
+    let autores = await fnGetAuthors();
+    let categorias = await fnGetCategories();
+    let editoriales = await fnGetEditorials();
+    let estados = await fnGetStates();
+    let selectsAutores = await Promise.all(crearOptions(autores));
+    let selectsCategorias = await Promise.all(crearOptions(categorias));
+    let selectsEditoriales = await Promise.all(crearOptions(editoriales));
+    let selectsEstados = await Promise.all(crearOptions(estados));
 
-    editorials.forEach((e) => {
-        selectEditorial.insertAdjacentHTML("beforeend", /*html*/`
-        <option value="${e.id}">${e.nombre}</option>
-        `)
-    })
+    return /*html*/`
+    <div>
+    <h4>Agregar Libro</h4>
+    <hr>
+    <form id="add-book-form">
+            <div class="form-group mb-0">
+                <label for="title-book">Titulo</label>
+                <input type="text" name="titulo" class="form-control" id="title-book" required>
+            </div>
+            <div class="form-group  mb-0">
+                <label for="isbn-book">ISBN</label>
+                <input type="text" name="isbn" class="form-control" id="isbn-book" required>
+            </div>
+            <div class="form-group  mb-0">
+                <label for="num-pg-book">Numero de Páginas</label>
+                <input type="text" name="num_paginas" class="form-control" id="num-pg-book"
+                    required>
+            </div>
+            <div class="form-group  mb-0">
+                <label for="url-book">URL Imagen Libro</label>
+                <input type="url" name="urlImg" class="form-control" id="url-book">
+            </div>
+            <div class="form-group  mb-0">
+                <label for="publication-date">Fecha de Publicación</label>
+                <input type="date" name="fecha_publicacion" class="form-control"
+                    id="publication-date" required>
+            </div>
+            <div class="form-group  mb-0">
+                <label for="category-book">Categoria</label>
+                <select class="form-control" id="category-book" name="categoriaId" required>
+                    <option value=""></option>
+                    ${selectsCategorias.join("")}
+                </select>
+            </div>
+            <div class="form-group  mb-0">
+                <label for="author-book">Autor</label>
+                <select class="form-control" id="author-book" name="autoreId" required>
+                    <option value=""></option>
+                    ${selectsAutores.join("")}
+                </select>
+            </div>
+            <div class="form-group  mb-0">
+                <label for="editorial-book">Editorial</label>
+                <select class="form-control" id="editorial-book" name="editorialeId" required>
+                    <option value=""></option>
+                    ${selectsEditoriales.join("")}
+                </select>
+            </div>
+            <div class="form-group  mb-0">
+                <label for="state-book">Estado</label>
+                <select class="form-control" id="state-book" name="estadoId" required>
+                    <option value=""></option>
+                    ${selectsEstados.join("")}
+                </select>
+            </div>
+        </form>
+        <div class="modal-footer">
+            <button type="button" class="btn-close-modal btn btn-secondary f-size-3">Cerrar</button>
+            <button type="submit" class="btn btn-primary f-size-3" form="add-book-form">Agregar</button>
+        </div>
+    </div>
+    `
+}
 
-    states.forEach((e) => {
-        selectState.insertAdjacentHTML("beforeend", /*html*/`
-        <option value="${e.id}">${e.nombre}</option>
-        `)
-    })
+export const formEditarLibro = async ({ book, fnGetAuthors, fnGetCategories, fnGetEditorials, fnGetStates }) => {
+    let autores = await fnGetAuthors();
+    let categorias = await fnGetCategories();
+    let editoriales = await fnGetEditorials();
+    let estados = await fnGetStates();
+    let selectsAutores = await Promise.all(crearOptions(autores));
+    let selectsCategorias = await Promise.all(crearOptions(categorias));
+    let selectsEditoriales = await Promise.all(crearOptions(editoriales));
+    let selectsEstados = await Promise.all(crearOptions(estados));
+
+    return /*html*/`
+    <div>
+    <h4>Editar Libro</h4>
+    <hr>
+    <form id="edit-book-form" data-edit="${book.id}">
+            <div class="form-group mb-0">
+                <label for="title-book">Titulo</label>
+                <input type="text" name="titulo" class="form-control" id="title-book" required value="${book.titulo}">
+            </div>
+            <div class="form-group  mb-0">
+                <label for="isbn-book">ISBN</label>
+                <input type="text" name="isbn" class="form-control" id="isbn-book" required value="${book.isbn}">
+            </div>
+            <div class="form-group  mb-0">
+                <label for="num-pg-book">Numero de Páginas</label>
+                <input type="text" name="num_paginas" class="form-control" id="num-pg-book" required value="${book.num_paginas}">
+            </div>
+            <div class="form-group  mb-0">
+                <label for="url-book">URL Imagen Libro</label>
+                <input type="url" name="urlImg" class="form-control" id="url-book" value="${book.urlImg || ""}">
+            </div>
+            <div class="form-group  mb-0">
+                <label for="publication-date">Fecha de Publicación</label>
+                <input type="date" name="fecha_publicacion" class="form-control" id="publication-date" required value="${book.fecha_publicacion}">
+            </div>
+            <div class="form-group  mb-0">
+                <label for="category-book">Categoria</label>
+                <select class="form-control" id="category-book" name="categoriaId" required>
+                    <option value=""></option>
+                    ${selectsCategorias.join("")}
+                </select>
+            </div>
+            <div class="form-group  mb-0">
+                <label for="author-book">Autor</label>
+                <select class="form-control" id="author-book" name="autoreId" required>
+                    <option value=""></option>
+                    ${selectsAutores.join("")}
+                </select>
+            </div>
+            <div class="form-group  mb-0">
+                <label for="editorial-book">Editorial</label>
+                <select class="form-control" id="editorial-book" name="editorialeId" required>
+                    <option value=""></option>
+                    ${selectsEditoriales.join("")}
+                </select>
+            </div>
+            <div class="form-group  mb-0">
+                <label for="state-book">Estado</label>
+                <select class="form-control" id="state-book" name="estadoId" required>
+                    <option value=""></option>
+                    ${selectsEstados.join("")}
+                </select>
+            </div>
+        </form>
+        <div class="modal-footer">
+            <button type="button" class="btn-close-modal btn btn-secondary f-size-3">Cerrar</button>
+            <button type="submit" class="btn btn-warning f-size-3" form="edit-book-form">Editar</button>
+        </div>
+    </div>
+    `
 }
 
 const numeroValido = (e) => {
@@ -89,5 +208,25 @@ export const eliminarLibro = async ({ id, fnDelLibro }) => {
         await fnDelLibro(numeroValido(id));
     } else {
         swalAlert({ type: 'error', title: "No fue posible eliminar." })
+    }
+}
+
+export const editarLibro = async ({ data, fnPutLibro }) => {
+    let res = await fnPutLibro({
+        "id": numeroValido(data.id),
+        "autoreId": numeroValido(data.autoreId),
+        "categoriaId": numeroValido(data.categoriaId),
+        "editorialeId": numeroValido(data.editorialeId),
+        "titulo": (data.titulo).toUpperCase(),
+        "fecha_publicacion": data.fecha_publicacion,
+        "isbn": (data.isbn).toLowerCase(),
+        "num_paginas": numeroValido(data.num_paginas),
+        "estadoId": numeroValido(data.estadoId),
+        "urlImg": data.urlImg ? data.urlImg : null
+    })
+    if ("id" in res) {
+        swalAlert({ type: 'success', title: "Agregado con exito." })
+    } else {
+        swalAlert({ type: 'error', title: "No fue posible agregar." })
     }
 }
